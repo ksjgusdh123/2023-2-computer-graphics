667a9c00ddcb0ff0fc42220bf7c0320f56732b20
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-09-16 03:07:52 +0900

    마우스 좌표계를 변환해주는 함수

---
+++ _마우스 좌표계변환.cpp
@@ -0,0 +1,12 @@
+float ox;
+float oy;
+
+GLvoid ConvertXY_OPENGL(int x, int y)
+{
+	int w = 800;
+	int h = 600;
+
+	ox = (float)(x - (float)w / 2.0) * (float)(1.0 / (float)(w / 2.0));
+	oy = -(float)(y - (float)h / 2.0) * (float)(1.0 / (float)(h / 2.0));
+
+}
